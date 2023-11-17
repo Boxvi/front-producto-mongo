ae349a2bcc90964ebb073ec886689e924e28c5ea
Boris <hermosokawai@gmail.com>
2023-11-17 17:29:05 -0500

    first commit

---
+++ src/services/ProductService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export class ProductService {
+
+    baseUrl = "https://api-producto-mongo.onrender.com/api/producto";
+
+    createProduct(producto) {
+        return axios.post(this.baseUrl, producto).then(res => res.data);
+    }
+
+    //READ
+    readAll() {
+        return axios.get(this.baseUrl).then(res => res.data);
+    }
+
+    //UPDATE
+    updateProduct(producto) {
+        return axios.put(this.baseUrl + "/" + producto.id, producto).then(res => res.data);
+    }
+
+    //DELETE
+    deleteProduct(_id) {
+        return axios.delete(this.baseUrl + "/" + _id).then(res => res.data);
+    }
+
+    findProductById(_id) {
+        return axios.get(this.baseUrl + "/" + _id).then(res => res.data);
+    }
+
+}
